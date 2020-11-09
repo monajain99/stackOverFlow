f174db86ef2ee9e41ed3970254aa7239c997e783
Rashmi <monajain99@hotmail.com>
2020-11-08 22:14:28 -0500

    wip

--- db/seeders/20201104010325-demoUser.js
+++ db/seeders/20201104010325-demoUser.js
@@ -11,7 +11,28 @@ module.exports = {
                 hashedPassword: "$2a$10$rp2T6gN3emCXuX4ezYju0u/5OvaROYtuNdmcvyD.IUstE48TP0xwu",
                 createdAt: new Date(),
                 updatedAt: new Date(),
-              }
+              },
+              {
+                username: 'sofiasaikh',
+                email: "sofiasaikh@sofiasaikh.com",
+                hashedPassword: "$2a$10$rp2T6gN3emCXuX4ezYju0u/5OvaROYtuNdmcvyD.IUstE48TP0xwu",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              },
+              {
+                username: 'janesmith',
+                email: "janesmith@janesmith.com",
+                hashedPassword: "$2a$10$rp2T6gN3emCXuX4ezYju0u/5OvaROYtuNdmcvyD.IUstE48TP0xwu",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              },
+              {
+                username: 'johnsmith',
+                email: "johnsmith@johnsmith.com",
+                hashedPassword: "$2a$10$rp2T6gN3emCXuX4ezYju0u/5OvaROYtuNdmcvyD.IUstE48TP0xwu",
+                createdAt: new Date(),
+                updatedAt: new Date(),
+              },
             ],
             {}
         );
